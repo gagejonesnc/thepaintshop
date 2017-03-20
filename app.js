9d4bea43fbcb29d892ff7ca7d27a718f56466d33
Chris Bradley <chris.bradley.423@gmail.com>
2017-03-19 20:39:20 -0400

    add lightbox feature to work page

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/node_modules/uikit/dist"));
+app.use(express.static(__dirname + "/node_modules/lightbox2/dist"));
 
 // include client-side assets and use the bodyParser
 app.use(express.static(__dirname + '/assets'));
